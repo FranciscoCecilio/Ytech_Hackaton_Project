43105e0cc573b18f0056e225af8b705fc0234138
Francisco Cecilio <ist189444@tecnico.ulisboa.pt>
2022-06-25 01:22:31 +0100

    score script

---
+++ Assets/Scripts/Score/Score.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Score
+{
+    static float _duration; // em segundos
+
+    public static void SetDuration(float duration){
+        _duration = duration;
+    }
+}

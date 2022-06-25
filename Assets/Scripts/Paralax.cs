30a25eda8f223470bbe9c13745f74a6ed66790e8
Francisco Cecilio <ist189444@tecnico.ulisboa.pt>
2022-06-25 05:46:32 +0100

    stash

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float scrollSpeed = 5f;
+    public float verticalSpeed = 1f;
+    public float verticalOffset = -3;
+    public float scrollOffset;
+    Vector3 startPos;
+	private float newPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+    void Update(){
+        newPos = Mathf.Repeat(Time.time * - scrollSpeed, scrollOffset);
+        transform.position = startPos + Vector3.right * newPos ;
+    }
+}
--- Assets/Scripts/Score/Score.cs
+++ Assets/Scripts/Score/Score.cs
@@ -6,7 +6,7 @@ using System;
 public static class Score
 {
     static float _duration; // em segundos
-    public static double _score; // em segundos
+    //public static double _score; // em segundos
 
     // chamado na scene do jogo
     public static void SetDuration(float duration){
@@ -16,16 +16,16 @@ public static class Score
     // chamado na scene do gameover
     public static void GameOver(){
         // calcular o score
-	    _score = Math.Pow(1.2f, _duration);
+	    //_score = Math.Pow(1.2f, _duration);
         // guardar o highscore nos playerprefs
         if(_duration > GetBestScore()){
             PlayerPrefs.SetFloat("highscore", _duration);
         }
     }
 
-    public static int GetBestScore(){
+    public static float GetBestScore(){
 
-        return PlayerPrefs.GetInt("highscore");
+        return PlayerPrefs.GetFloat("highscore");
     }
 
 
